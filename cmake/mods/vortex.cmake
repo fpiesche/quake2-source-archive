4500ed96fbe78d2ee3e88ae6b695ca69e034c31d
Florian Piesche <florian@yellowkeycard.net>
2025-07-01 18:03:18 +0100

    Try and fix...

--- cmake/mods/vortex.cmake
+++ cmake/mods/vortex.cmake
@@ -9,8 +9,5 @@ file(GLOB MOD_SRC
 
 add_library(${MOD_NAME} SHARED ${MOD_SRC})
 set_target_properties(${MOD_NAME} PROPERTIES PREFIX "")
-set_target_properties(${MOD_NAME} PROPERTIES OUTPUT_NAME "bin/${MOD_NAME}/${OS_HANDLE}/game${Q2_BUILD_ARCH}")
-target_include_directories(
-  ${MOD_NAME}
-  PRIVATE
-  "sources/${MOD_NAME}/src/src")
+set_target_properties(${MOD_NAME} PROPERTIES OUTPUT_NAME "${CMAKE_BINARY_DIR}/${MOD_NAME}/${OS_HANDLE}/game${Q2_BUILD_ARCH}")
+target_include_directories(${MOD_NAME} PRIVATE "sources/${MOD_NAME}/src/src")
